76a25cdc1c18ee0e6fd763ff691981e2763a2224
igor.sobral <igor.sobral@pagtel.com.br>
2021-12-29 23:53:19 -0300

    chore: create cart icon component

---
+++ components/CartIcon/index.tsx
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+import { CartContext } from '../../context/cardContent'
+
+export  function CartIcon({ navigation }: any)  {
+    const { getItemsCount } = useContext(CartContext);
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text} onPress={() =>{
+                navigation.navigate("Cart")
+            }}>
+                Carrinho ({getItemsCount()})
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginHorizontal: 8,
+        backgroundColor: "#515b8c",
+        height: 40,
+        padding: 15,
+        borderRadius: 5,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    text: {
+        color: "#fff",
+        fontWeight: "normal",
+    }
+})
